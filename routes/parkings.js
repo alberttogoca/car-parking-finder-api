2f4d9b887fbade511ecebfac9153eaceb7319eed
alberttogoca <2abgomez@gmail.com>
2019-11-15 20:25:48 +0200

    Add Parkings Data

---
+++ routes/parkings.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+//PARKINGS SPOTS
+const parkings = [
+  {
+    id: 1,
+    title: 'Parking 1',
+    price: 5,
+    rating: 4.2,
+    free: false,
+    coordinate: {
+      latitude: 60.201136,
+      longitude: 24.935004,
+    },
+    description: `Description about this parking lot
+
+Open year 2018
+Secure with CTV`,
+  },
+  {
+    id: 2,
+    title: 'Parking 2',
+    price: 7,
+    rating: 3.8,
+    free: true,
+    coordinate: {
+      latitude: 60.202219,
+      longitude: 24.933888,
+    },
+    description: `Description about this parking lot
+
+Open year 2014
+Secure with CTV`,
+  },
+  {
+    id: 3,
+    title: 'Parking 3',
+    price: 10,
+    rating: 4.9,
+    free: true,
+    coordinate: {
+      latitude: 60.201611,
+      longitude: 24.937697,
+    },
+    description: `Description about this parking lot
+
+Open year 2019
+Secure with CTV`,
+  },
+];
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(parkings);
+});
+
+module.exports = router;
